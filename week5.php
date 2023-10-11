bb189e86d8beffee88ee4c309aec4e35c3bfbab6
sarwatkochar <145318077+sarwatkochar@users.noreply.github.com>
2023-10-11 09:19:24 +0300

    Add files via upload

---
+++ week5.php
@@ -0,0 +1,35 @@
+<?php
+
+$car = array("kia" , "BMW" ,"TOYOTA");
+echo count($cars);
+
+
+$car = array("kia" , "BMW" ,"TOYOTA");
+$arrlength = count ($cars);
+
+
+for(x=0 ; $x < $arrlength; $x++) {
+Echo $cars[$x];
+echo "<br>"
+
+
+$age = arrayl"Ahmed"=>"35", "Sara"=>"37", "Safin"=>"43");
+
+$age["Ahmed "] = "35";
+$age["Sara"] = "37";
+$age "Safin"] = "43"; 
+
+}
+
+$cars = array("kia" , "toyota" , "BMW" , "Mercedes");
+ sort($cars);
+    $clength = count ($cars) ; 
+    for($x=0 ;$x < $clength; $x++ ){
+        echo $cars[$x];
+        echo"<br>"
+
+    }
+
+
+
+?>
